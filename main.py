51e28c719fa51653dabacdd608a9d5185be90c83
Kjm478 <you@example.com>
2025-03-11 21:39:03 -0400

    used langflow to create the follow for the system and called the macros using the API

---
+++ main.py
@@ -0,0 +1,69 @@
+from langflow.load import run_flow_from_json
+from dotenv import load_dotenv
+import requests
+from typing import Optional
+import os
+
+BASE_API_URL = "https://api.langflow.astra.datastax.com"
+APPLICATION_TOKEN = os.getenv("APPLICATION_TOKEN")
+
+
+load_dotenv()
+
+def get_askai(question, profile): 
+  
+  LANGFLOW_ID = "24b19386-5be7-48db-97af-c55ad05efe6b"
+  ENDPOINT = "askai" # The endpoint name of the flow
+  
+  TWEAKS = {
+    "TextInput-fanPK": {
+      "input_value": question
+    },
+    "TextInput-CXuEG": {
+      "input_value": profile
+    },
+    }
+  return run_flow("", endpoint = ENDPOINT,langflow_id= LANGFLOW_ID,  tweaks=TWEAKS, application_token=APPLICATION_TOKEN)
+
+def get_macros(profile, goals):
+  TWEAKS = {
+    "TextInput-3YLwx": {
+      "input_value": goals
+    }, 
+    "TextInput-niwMA": {
+      "input_value": profile
+    },
+    }
+  return run_flow("", endpoint = "macros",langflow_id= "24b19386-5be7-48db-97af-c55ad05efe6b",  tweaks=TWEAKS, application_token=APPLICATION_TOKEN)
+
+def run_flow(message: str,
+  endpoint: str,
+  langflow_id: str, 
+  output_type: str = "chat",
+  input_type: str = "chat",
+  tweaks: Optional[dict] = None,
+  application_token: Optional[str] = None) -> dict:
+    api_url = f"{BASE_API_URL}/lf/{langflow_id}/api/v1/run/{endpoint}"
+
+    payload = {
+        "input_value": message,
+        "output_type": output_type,
+        "input_type": input_type,
+    }
+    headers = None
+    if tweaks:
+        payload["tweaks"] = tweaks
+    if application_token:
+        headers = {"Authorization": "Bearer " + application_token, "Content-Type": "application/json"}
+    response = requests.post(api_url, json=payload, headers=headers)
+  
+    return response.json()["outputs"][0]["outputs"][0]['results']["text"]["data"]["text"]
+
+
+result = get_macros("name: Kennedy , age: 27 , weight: 80kgs, 175cm", "muscle gain")  
+print(result)
+print("\n")
+result = get_askai("What is the best workout for muscle gain?", "name: Kennedy , age: 27 , weight: 80kgs, 175cm")
+print(result)
+
+

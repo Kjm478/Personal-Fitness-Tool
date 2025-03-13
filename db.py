baed224bc999b1393fd1f5a8ed86c1472a6b406f
Kjm478 <you@example.com>
2025-03-12 20:05:25 -0400

    connected to the end point of the astra db through the API

---
+++ db.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from astrapy import DataAPIClient
+import streamlit as st 
+import os 
+
+
+load_dotenv()
+
+endpoint = os.getenv("endpoint_astra")
+token = os.getenv("astra_db_token")
+
+@st.cache_resource
+def get_db():
+    client = DataAPIClient(token)
+    db = client.get_database_by_api_endpoint(endpoint)
+    return db
+
+db = get_db()
+collection_names = ["personal_data", "notes"]
+
+for name in collection_names:
+    try:
+       db.create_collection(name)
+    except:
+       pass
+        
+personal_data = db.get_collection("personal_data")
+notes = db.get_collection("notes")
+
+print(db.list_collection_names())
